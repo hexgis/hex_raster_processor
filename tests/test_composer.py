0fe02c3926032abf91dc88f4a2241faed3d163f3
dagnaldo <naldo.penha@gmail.com>
2021-06-14 13:22:16 -0300

    Adding test to create composition with error bands.

--- tests/test_composer.py
+++ tests/test_composer.py
@@ -6,6 +6,7 @@ import pytest
 
 from hex_raster_processor.composer import Composer
 from hex_raster_processor.utils import Utils
+from hex_raster_processor.exceptions import ValidationBandError
 
 from .test_base import download_images
 
@@ -46,3 +47,24 @@ def test_create_composition_with_downloaded_images(get_default_data):
 
     assert(composition["type"] == "r6g5b4")
     assert(composition["name"] == "{}_r6g5b4.TIF".format(product))
+
+
+def test_create_composition_with_downloaded_images_error(get_default_data):
+    """ Test download and create composition with downloaded images error """
+
+    product = get_default_data.get("product")
+    output_dir = get_default_data.get("output_dir")
+
+    download = download_images(bands=[6, 5, 4])
+    assert(len(download) == 5)
+
+    test_error = [download[0], download[1], 'test_error.tif']
+
+    with pytest.raises(ValidationBandError):
+        Composer.create_composition(
+            filename=product,
+            ordered_filelist=test_error,
+            output_path=Utils.check_creation_folder(output_dir),
+            bands=[6, 5, 4],
+            quiet=QUIET
+        )
